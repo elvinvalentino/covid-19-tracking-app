c4e91d92c6c8764df57f0b32d74ae4123676acd1
Elvin Valentino <elvin.valentino@gmail.com>
2020-03-28 13:30:28 +0700

    adjust my footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,17 @@ const Footer = () => {
         <div className="footer">
             <span>copyright &copy; 2020 vinval.com. All rights reserved</span>
             <div className="social-bar">
-                <a href="/" className="social-icon">
-                    <img src="https://img.icons8.com/color/48/000000/facebook-new.png" alt=""/>
+                <a href="https://www.facebook.com/elvin.valentino" target="__blank" className="social-icon">
+                    <img src="https://img.icons8.com/color/48/000000/facebook-new.png" alt="facebook"/>
                 </a>
-                <a href="/" className="social-icon">
-                    <img src="https://img.icons8.com/color/48/000000/twitter.png" alt=""/>
+                <a href="https://twitter.com/ElvinValentino" target="__blank" className="social-icon">
+                    <img src="https://img.icons8.com/color/48/000000/twitter.png" alt="twitter"/>
                 </a>
-                <a href="/" className="social-icon">
-                    <img src="https://img.icons8.com/color/48/000000/gmail.png" alt=""/>
+                <a href="/" className="social-icon" data-title="elvin.valentino@gmail.com">
+                    <img src="https://img.icons8.com/color/48/000000/gmail.png" alt="gmail"/>
                 </a>
-                <a href="/" className="social-icon">
-                    <img src="https://img.icons8.com/color/48/000000/instagram-new.png" alt=""/>
+                <a href="https://www.instagram.com/elvin.val/" target="__blank" className="social-icon">
+                    <img src="https://img.icons8.com/color/48/000000/instagram-new.png" alt="instagram"/>
                 </a>
             </div>
         </div>
