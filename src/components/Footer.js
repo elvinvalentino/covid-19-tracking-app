b2078cb8725ffe760bc45a58e9b4e7223b620603
Elvin Valentino <57981550+elvinvalentino@users.noreply.github.com>
2020-11-07 10:33:25 +0700

    change twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,8 @@ const Footer = () => {
                 <a href="https://www.facebook.com/elvin.valentino" target="__blank" className="social-icon">
                     <img src="https://img.icons8.com/color/48/000000/facebook-new.png" alt="facebook"/>
                 </a>
-                <a href="https://twitter.com/ElvinValentino" target="__blank" className="social-icon">
+                <a href="https://twitter.com/elvin_val" target="__blank" className="social-icon">
                     <img src="https://img.icons8.com/color/48/000000/twitter.png" alt="twitter"/>
-                </a>
-                <a href="/" className="social-icon" data-title="elvin.valentino@gmail.com">
-                    <img src="https://img.icons8.com/color/48/000000/gmail.png" alt="gmail"/>
                 </a>
                 <a href="https://www.instagram.com/elvin.val/" target="__blank" className="social-icon">
                     <img src="https://img.icons8.com/color/48/000000/instagram-new.png" alt="instagram"/>
