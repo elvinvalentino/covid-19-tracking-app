dc708818216010832f4a9dc433d4195ba90ce6a3
Elvin Valentino <57981550+elvinvalentino@users.noreply.github.com>
2020-11-07 10:21:39 +0700

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -27,7 +27,9 @@ class Dashboard extends Component {
             this.setState({
                 cases: data
             }, () => {
-                this.componentDidMount();
+                setTimeOut(() => {
+                    this.componentDidMount();
+                }, 300000);
             });
         });
     })
